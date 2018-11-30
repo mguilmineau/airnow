ee9f310590b9af69d5625cf024c3b0a8bede84db
Mathieu Guilmineau <mguilmineau@gmail.com>
2018-11-29 20:34:30 -0800

    invalid configuration of private.exs - was not being interpreted at compile time

--- config/config.exs
+++ config/config.exs
@@ -1,23 +1,15 @@
 use Mix.Config
 
-# load full name, username and password from private config file
-# config :airnow, name: "..."
-# config :airnow, email_username: "..."
-# config :airnow, email_password: "..."
-import_config "private.exs"
-
-config :airnow, admin_user: %{ name: Application.get_env( :airnow, :name ), email: Application.get_env( :airnow, :email_username ) }
-config :airnow, default_tmp: "d:/tmp" # yes, I developed this on windows...
-config :airnow, default_zip: "95051"
-config :airnow, url: "https://www.airnow.gov"
-# https://www.whoishostingthis.com/tools/user-agent/
-config :airnow, user_agent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36"
+config :airnow,
+	default_tmp: "d:/tmp", # yes, I developed this on windows...
+	default_zip: "95051",
+	url: "https://www.airnow.gov",
+	# https://www.whoishostingthis.com/tools/user-agent/
+	user_agent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36"
 
 config :airnow, AirNow.Mailer,
   adapter: Swoosh.Adapters.SMTP,
   relay: "smtp.gmail.com",
-  username: Application.get_env( :airnow, :email_username ),
-  password: Application.get_env( :airnow, :email_password ),
   ssl: true,
   tls: :never, # bad record mac when on port 587 with TLS - appears to be a known erlang SSL issue
   auth: :always,
@@ -25,6 +17,21 @@ config :airnow, AirNow.Mailer,
   retries: 2,
   no_mx_lookups: false
 
-config :hound, driver: "chrome_driver", browser: "chrome_headless"
-config :hound, retry_time: 500
-config :hound, genserver_timeout: 480000
+config :hound,
+	driver: "chrome_driver",
+	browser: "chrome_headless",
+	retry_time: 500,
+	genserver_timeout: 480000
+
+# load full name, username and password from private.exs config file
+#
+#config :airnow,
+#	name: "...",
+#	email_username: "...",
+#	email_password: "..."
+#
+#config :airnow, AirNow.Mailer,
+#  username: "...",
+#  password: "..."
+
+import_config "private.exs"
